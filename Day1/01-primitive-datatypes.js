d3daaa3b094a3fd14d474481ecfbae75146cfb85
MadhankumarVanaraj <kumar.madhanmca@gmail.com>
2025-09-25 10:15:59 +0530

    Add files via upload

---
+++ Day1/01-primitive-datatypes.js
@@ -0,0 +1,31 @@
+/* 1) Assignment Details
+Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`,
+including an uninitialized variable */
+
+let firstname = "Madhankumar Vanaraj";
+console.log(firstname);
+console.log(typeof(firstname));
+console.log("");
+
+let Companyname = "Boston Harbor Consulting.";
+console.log(Companyname);
+console.log(typeof(Companyname));
+console.log("");
+
+let MobileNumber = 8015766771;
+console.log(MobileNumber);
+console.log(typeof(MobileNumber));
+console.log("");
+
+let isAutomation = true;
+console.log(isAutomation);
+console.log(typeof(isAutomation));
+console.log("");
+
+let hasplaywright
+console.log(hasplaywright);
+console.log(typeof(hasplaywright));
+console.log(""); 
+
+
+
---
+++ Day1/variables-and-data-types.js
@@ -0,0 +1,28 @@
+// Variables and Data types
+
+// Number DataType
+let number = 12
+console.log(typeof(number)); //OUtput: number
+
+//String Data Type 
+let Product = "Quickly";
+console.log(typeof(Product)); //Output: string
+
+//Boolean Data Type
+let Status = true;
+console.log(typeof(Status)); //Output: boolean
+
+//Big Int Data Type
+let AccountNumber = 910035412568756n
+console.log(typeof(AccountNumber));
+console.log(""); //Output: bigint
+
+//Undefined DataType
+let AcNumber;
+console.log(AcNumber);
+console.log(typeof(AcNumber)); //Output: undefined 
+
+//Null DataType
+let Landlinenumber = null
+console.log(Landlinenumber);
+console.log(typeof(Landlinenumber)); //Output: Object 
